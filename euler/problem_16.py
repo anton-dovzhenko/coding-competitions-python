de3f80f79636cd37cd40b127266feca78ae13f6c
Anton <anthony300787@gmail.com>
2020-04-13 21:29:38 +0800

    Euler-16

--- euler/problem_16.py
+++ euler/problem_16.py
@@ -1,10 +1,5 @@
 class Solution:
 
     def get_power_digit_sum(self, n):
-        digits = 2 ** n
-        digits = str(digits)
-        _sum = 0
-        for d in digits:
-            _sum += ord(d) - ord('0')
-        return _sum
+        return sum([int(d) for d in str(2 ** n)])
 
