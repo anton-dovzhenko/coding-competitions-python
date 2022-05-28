996384ae02c483de3e4a16f703691aa0b74a9f15
anton <anthony300787@gmail.com>
2022-05-28 16:06:52 +0800

    Leetcode-2279

---
+++ leetcode/_2279_Maximum_Bags_With_Full_Capacity_of_Rocks.py
@@ -0,0 +1,26 @@
+import unittest
+from typing import List
+
+
+class Solution:
+
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        for i in range(len(capacity)):
+            capacity[i] -= rocks[i]
+        capacity.sort()
+        bags = 0
+        for c in capacity:
+            if additionalRocks - c >= 0:
+                bags += 1
+                additionalRocks -= c
+            else:
+                break
+        return bags
+
+
+class SolutionTest(unittest.TestCase):
+
+    def test_latestDayToCross(self):
+        self.assertEquals(Solution().maximumBags([2, 3, 4, 5], [1, 2, 4, 4], 2), 3)
+        self.assertEquals(Solution().maximumBags([10, 2, 2], [2, 2, 0], 100), 3)
+
