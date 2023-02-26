5e990a210fa8add4e7e2e00abd23b33f2f0790fb
anton <anthony300787@gmail.com>
2023-02-26 21:50:08 +0800

    leetcode-2501

---
+++ leetcode/_2501_Longest_Square_Streak_in_an_Array.py
@@ -0,0 +1,27 @@
+import unittest
+from typing import List
+
+
+class Solution:
+
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        nums = set(nums)
+        longest = -1
+        for num in nums:
+            n = num
+            temp = 1
+            while n ** 2 in nums:
+                n = n ** 2
+                temp += 1
+                longest = max(longest, temp)
+
+        return longest
+
+
+class SolutionTest(unittest.TestCase):
+
+    def test_longestSquareStreak(self):
+        self.assertTrue(Solution().longestSquareStreak([4, 3, 6, 16, 8, 2]), 3)
+        self.assertTrue(Solution().longestSquareStreak([2, 3, 5, 6, 7]), -1)
+
+
