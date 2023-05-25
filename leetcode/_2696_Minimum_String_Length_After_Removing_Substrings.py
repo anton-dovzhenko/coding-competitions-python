7e92ea785e466cd2eeb1ee945dd087d26ace6db3
anton <anthony300787@gmail.com>
2023-05-25 10:53:35 +0800

    leetcode-2696

---
+++ leetcode/_2696_Minimum_String_Length_After_Removing_Substrings.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution:
+
+    def minLength(self, s: str) -> int:
+        n = len(s)
+        while True:
+            s = s.replace('AB', '')
+            s = s.replace('CD', '')
+            if n == len(s):
+                break
+            else:
+                n = len(s)
+        return n
+
+
+class SolutionTest(unittest.TestCase):
+
+    def test_getSubarrayBeauty(self):
+        self.assertEqual(Solution().minLength("ABFCACDB"), 2)
+        self.assertEqual(Solution().minLength("ACBBD"), 5)
+
+
